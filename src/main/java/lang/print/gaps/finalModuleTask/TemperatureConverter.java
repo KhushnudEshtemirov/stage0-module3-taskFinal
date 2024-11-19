8d53f5383c4721969c3e8e72c3dc2b24bd92310c
Xushnud Eshtemirov <xushnud122997@gmail.com>
2024-11-19 09:10:41 +0500

    Added output operation for temperature converter file

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -3,5 +3,6 @@ package lang.print.gaps.finalModuleTask;
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
         double fahrenheit = temperatureCelsius * 1.8 + 32;
+        System.out.println(fahrenheit);
     }
 }
